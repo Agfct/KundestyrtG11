c0058ff25e313461ef0a849f667409e254463a15
EirikZimmerWold <eirikzimmer@hotmail.com>
2015-09-29 20:10:27 +0200

    added a few functions to timelineModel

--- ScreenTeamFX/src/modules/TimelineModel.java
+++ ScreenTeamFX/src/modules/TimelineModel.java
@@ -1,15 +1,18 @@
 package modules;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TimelineModel {
 	
 	private ArrayList<MediaObject> mediaObjects;
 	private final int id;
+	private ArrayList<Event> timelineStack;
 	public TimelineModel(ArrayList<MediaObject> mediaObjects, int id) {
 		super();
 		this.mediaObjects = mediaObjects;
 		this.id = id;
+		this.timelineStack = new ArrayList<Event>();
 	}
 
 	public ArrayList<MediaObject> getMediaObjects() {
@@ -22,6 +25,32 @@ public class TimelineModel {
 
 	public int getID() {
 		return id;
+	}
+	
+	public void addMediaObject(MediaObject m){
+		mediaObjects.add(m);
+		timelinechanged();
+	}
+	
+	public void removeMediaObject(MediaObject m){
+		mediaObjects.add(m);
+		timelinechanged();
+	}
+	public void timelinechanged(){
+		timelineStack.clear();
+		MediaObject mO;
+		Event start;
+		Event end;
+		for(int i=0;i>=mediaObjects.size();i++){
+			mO = mediaObjects.get(i);
+			if (mO instanceof MediaObjectVideo){
+				start = new Event(((MediaObjectVideo) mO).getStartTime(), id, Action.PLAY);
+				end = new Event(((MediaObjectVideo) mO).getEndVideo(), id, Action.PLAY);
+				timelineStack.add(start);
+				timelineStack.add(end);
+			}
+		Collections.sort(timelineStack);
+		}
 	}
 		
 }
