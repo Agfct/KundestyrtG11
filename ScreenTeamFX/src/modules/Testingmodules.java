e779af5867cb622a3689e5ac25f5d2487c72670d
EirikZimmerWold <eirikzimmer@hotmail.com>
2015-10-12 16:26:29 +0200

    tested pauseOne (failed)

--- ScreenTeamFX/src/modules/Testingmodules.java
+++ ScreenTeamFX/src/modules/Testingmodules.java
@@ -14,11 +14,24 @@ public class Testingmodules {
 		MediaObject mO = new MediaObject("C:\\Users\\EirikZimmer\\Videos\\video_test_512kb.mp4","test",MediaSourceType.VIDEO);
 		tlmodul.addDisplay(0);
 		tlmodul.assignTimeline(0, tlm);
-		tlm.addTimelineMediaObject(1000, 4000, mO);
-		tlm.addTimelineMediaObject(6000,10000,mO);
-		tlm.getTimelineMediaObjects().get(0).setStartPoint(10000);
+		tlm.addTimelineMediaObject(1000, 15000, mO);
+		//tlm.addTimelineMediaObject(6000,10000,mO);
+		//tlm.getTimelineMediaObjects().get(0).setStartPoint(10000);
 		tlmodul.playOne(0,0);
-		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		tlmodul.pauseOne(0);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		tlmodul.playOne(0, 2000);
 	}
 	public static void main(String[] args) {
 		Testingmodules kk = new Testingmodules();
