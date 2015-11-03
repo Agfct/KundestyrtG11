0baea41b2e9d1424f932dab96c56ee4f2d53edc1
magnusgundersen <magnusgund@gmail.com>
2015-11-03 14:02:10 +0100

    Added textFields and labels to the timelineInfo

--- ScreenTeamFX/src/modules/TimelineModel.java
+++ ScreenTeamFX/src/modules/TimelineModel.java
@@ -17,12 +17,15 @@ public class TimelineModel implements Serializable{
 	
 	private ArrayList<Integer> assignedDisplays;
 	
+	private String nameOfTimeline;
+	
 	public TimelineModel(int id) {
 		super();
 		this.timelineMediaObjects = new ArrayList<TimelineMediaObject>();
 		this.id = id;
 		this.timelineStack = new ArrayList<Event>();
 		this.assignedDisplays=new ArrayList<Integer>();
+		this.nameOfTimeline= "Timeline " + id;
 		
 	}
 
@@ -268,6 +271,18 @@ public class TimelineModel implements Serializable{
 
 	public void removeAllDisplays() {
 		assignedDisplays = new ArrayList<Integer>(); 
+	}
+	
+	public String getNameOfTimline(){
+		return nameOfTimeline;
+	}
+	
+	public boolean setNameOfTimeline(String s){
+		if(s.length()<20){
+			nameOfTimeline=s;
+			return true;
+		}
+		return false;
 	}
 		
 }
