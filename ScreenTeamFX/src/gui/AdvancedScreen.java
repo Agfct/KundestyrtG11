a46edae98ab29715d370ce3eed063afb52586f56
AndersLunde <agfct@hotmail.com>
2015-11-04 21:06:43 +0100

    Added a lot of minor fixes to modal and other component

--- ScreenTeamFX/src/gui/AdvancedScreen.java
+++ ScreenTeamFX/src/gui/AdvancedScreen.java
@@ -665,7 +665,7 @@ public class AdvancedScreen implements Screen{
 			modalDialog.initModality(Modality.APPLICATION_MODAL);
 			modalDialog.initOwner(MainGUIController.getInstance().primaryStage);
 			ModalController mediaObjectModal = new ModalController(mediaObject);
-			Scene dialogScene = new Scene(mediaObjectModal.getRoot(), 500, 230);
+			Scene dialogScene = new Scene(mediaObjectModal.getRoot(), 600, 230);
 			modalDialog.setScene(dialogScene);
 			modalDialog.show();
 		}
